de9dd6978f7a80ad74c04db0463c843ae4a2c9c5
Law Horne <foxtrottwist@ymail.com>
2019-06-26 22:41:30 -0400

    Added react-testing-library

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import { render } from '@testing-library/react'
+import App from './App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+it('renders with welcome message', () => {
+  const { getByText } = render(<App />)
+  expect(getByText('What Would You Like to Learn Today?'))
+})
