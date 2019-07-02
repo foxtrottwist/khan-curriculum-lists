1f476d00eb1f40722b9ffd8951a67cc2a4113ad9
Law <law.horne@live.com>
2019-07-02 14:02:25 -0400

    Finished happy path test

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import { cleanup, render } from '@testing-library/react'
+import 'jest-dom/extend-expect'
 import App from './App'
+
+import { SUBJECTS } from './utils/constants'
 
 afterEach(cleanup)
 
 describe('App', () => {
-  it('renders with welcome message', () => {
-    const { debug, getByText } = render(<App />)
+  it('renders with welcome message and subject buttons', () => {
+    const { getAllByRole, getByText } = render(<App />)
+
+    const buttons = getAllByRole('button').map(element => element.textContent)
+
+    expect(buttons).toEqual(SUBJECTS.listing)
     expect(getByText('What Would You Like to Learn Today?'))
-    debug()
   })
 })
